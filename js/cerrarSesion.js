4f51d07fe1e85c89b73dff91df9af9dc0dda8e0c
Hector_Fonseca <fonsecahesilva@gmail.com>
2020-05-04 02:39:57 -0600

    Empece formulario Nuevo cliente con boostrapp

--- js/cerrarSesion.js
+++ js/cerrarSesion.js
@@ -5,11 +5,14 @@ function cerrarSesion() {
     firebase.auth().signOut()
         .then(function () {
             console.log('salimos')
+            
             location.href = '../index.html'
+           
         })
         .catch(function (error) {
             console.log(error)
 
         })
+
 
 }
